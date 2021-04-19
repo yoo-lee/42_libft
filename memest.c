7587006bd083eb958487b91ae10f8671b1822345
yoo-lee <yuuuukan@gmail.com>
2021-04-19 20:44:09 +0900

    1

---
+++ memest.c
@@ -0,0 +1,11 @@
+#include <libft.h>
+
+void ft_memset(void *b, int c, size_t leng)
+{
+    unsigned char *ptr
+
+ptr= (unsigned char*)b;
+while (length-- > 0)
+*(ptr++) = (unsigned chat)c;
+return (b);
+}
