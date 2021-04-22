e6e79c9942bd84fdf0626fd676963a6162d2ef18
yoo-lee <yuuuukan@gmail.com>
2021-04-22 10:54:56 +0900

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,23 +1,21 @@
-#include "libft.h"
-
-int
-	ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-	int	i;
-	int	is_neg;
-	int	res;
+    int	     res;
+    int	negative;
 
-	if (!str)
-		return (0);
-	i = 0;
-	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' ||
-			str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
-		i++;
-	is_neg = (str[i] == '-') ? -1 : 1;
-	if (is_neg == -1 || str[i] == '+')
-		i++;
+	negative = 1;
 	res = 0;
-	while (str[i] >= '0' && str[i] <= '9')
-		res = (res * 10) + (str[i++] - '0');
-	return (res * is_neg);
+	while (*str && (*str == ' ' || *str == '\n' || *str == '\t' 
+			|| *str == '\v' || *str == '\f' || *str == '\r'))
+		++str;
+	if (*str == '-')
+		negative = -1;
+	if (*str == '-' || *str == '+')
+		++str;
+	while (*str && *str >= '0' && *str <= '9')
+	{
+		res = res * 10 + (*str - 48);
+		++str;
+    }
+    return (res * negative);
 }
