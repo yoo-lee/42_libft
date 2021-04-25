a236da2b6906379d42be0aa8e8955943ea732066
Yoo hwan Lee <yoo-lee@c1r1s6.42tokyo.jp>
2021-04-25 16:54:14 +0900

    15

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: yoo-lee <yoo-lee@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/23 12:13:03 by yoo-lee           #+#    #+#             */
-/*   Updated: 2021/04/23 12:18:08 by yoo-lee          ###   ########.fr       */
+/*   Updated: 2021/04/25 12:19:42 by yoo-lee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ void
 	ptr = (void *)malloc(count * size);
 	if (!ptr)
 		return (NULL);
+	if (ptr == 0)
+		return (0);
 	ft_bzero(ptr, count);
 	return (ptr);
 }
