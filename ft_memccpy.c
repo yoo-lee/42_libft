97873772ef5bb22002229d5080047d73c1ad58b6
Yoo hwan Lee <yoo-lee@c1r9s1.42tokyo.jp>
2021-04-25 23:26:17 +0900

    16

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -5,29 +5,32 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yoo-lee <yoo-lee@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/21 11:23:23 by yoo-lee           #+#    #+#             */
-/*   Updated: 2021/04/23 15:32:25 by yoo-lee          ###   ########.fr       */
+/*   Created: 2021/04/25 22:29:01 by yoo-lee           #+#    #+#             */
+/*   Updated: 2021/04/25 22:29:13 by yoo-lee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+void    *ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
-	size_t		i;
-	char		*dest;
-	char		*sourc;
+    unsigned char    *cpy_src;
+    unsigned char    *cpy_dest;
 
-	i = 0;
-	dest = dst;
-	sourc = (char *)src;
-	while (i < n && (i == 0 || sourc[i - 1] != c))
-	{
-		dest[i] = sourc[i];
-		i++;
-	}
-	if (i > 0 && sourc[i - 1] == c)
-		return (dest + i);
-	else
-		return (NULL);
+    if (!dest && !src)
+        return (NULL);
+    cpy_src = (unsigned char*)src;
+    cpy_dest = (unsigned char*)dest;
+    while (n-- > 0)
+    {
+        *cpy_dest = *cpy_src;
+        if (*cpy_src == (unsigned char)c)
+        {
+            return (++cpy_dest);
+        }
+        cpy_dest++;
+        cpy_src++;
+    }
+    return (NULL);
 }
+
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: yoo-lee <yoo-lee@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 13:59:16 by yoo-lee           #+#    #+#             */
-/*   Updated: 2021/04/21 14:19:03 by yoo-lee          ###   ########.fr       */
+/*   Updated: 2021/04/25 23:24:41 by yoo-lee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,3 +27,7 @@ void
 	}
 	return (dst);
 }
+//int main(void)
+//{
+//	ft_memcpy(((void *)0), "segfaulter tu dois", 17);
+//}
